20b7a45be07948f7fd3b04ce77b60be579126404
iamtonmoy0 <tonmoy2code@gmail.com>
2024-02-16 20:32:59 +0600

    db configured

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
+import db from "./config/database.config";
+
+// db connection
+db.sync().then(() => {
+  console.log("Connected to db successfully");
+});
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({he:"ok"});
+  res.status(200).json({ he: "ok" });
 });
 
 // listening port
